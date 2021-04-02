2410f7d35799efb48ef84be94639542fe8db8961
souhaiLebbar <souhail.lebbar@etu.unice.fr>
2021-04-02 07:36:02 +0200

    all links set almost done with first part

---
+++ app/src/main/java/com/polytech/ihm/activities/demand/Favorite.java
@@ -0,0 +1,17 @@
+package com.polytech.ihm.activities.demand;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.polytech.ihm.R;
+
+public class Favorite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite);
+    }
+
+}
