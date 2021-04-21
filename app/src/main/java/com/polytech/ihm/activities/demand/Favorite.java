962fa4c7d68153846a6a2fcc8f514c566a38d4d0
souhaiLebbar <souhai.lebbar@etu.unice.fr>
2021-04-21 15:45:15 +0200

    ajout des favorie

--- app/src/main/java/com/polytech/ihm/activities/demand/Favorite.java
+++ app/src/main/java/com/polytech/ihm/activities/demand/Favorite.java
@@ -3,6 +3,7 @@ package com.polytech.ihm.activities.demand;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.polytech.ihm.R;
 
@@ -12,6 +13,20 @@ public class Favorite extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
+        //Récupération de la liste de pizzas (peuplée)
+        ListPizza pizzas = new ListPizza();
+
+        //Création et initialisation de l'Adapter pour les diplomes
+        PizzasAdapter adapter = new PizzasAdapter(getApplicationContext(), pizzas);
+
+        //Récupération du composant ListView
+        ListView list = findViewById(R.id.listView);
+
+        //Initialisation de la liste avec les données
+        list.setAdapter(adapter);
+
+        //écouter si on clique
+        adapter.addListner(this);
     }
 
 }
